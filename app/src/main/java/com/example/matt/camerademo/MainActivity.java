127d7fc7511d4b586bfd4953396a6590949e2b91
misliefson <misliefson57@academic.rrc.ca>
2017-11-08 15:14:20 -0600

    Added Saving Image

--- app/src/main/java/com/example/matt/camerademo/MainActivity.java
+++ app/src/main/java/com/example/matt/camerademo/MainActivity.java
@@ -9,19 +9,33 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
 
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCamera;
     private boolean hasCamera = false;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +44,19 @@ public class MainActivity extends AppCompatActivity {
 
         hasCamera = checkCameraHardware(getApplicationContext());
 
-        if(hasCamera)
-            openCamera();
-        else
-        {
-            Toast.makeText(MainActivity.this, "Camera Not Available on Device", Toast.LENGTH_LONG).show();
-        }
+        btnCamera = (Button) findViewById(R.id.btnCamera);
 
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(hasCamera)
+                    openCamera();
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Camera Not Available on Device", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private boolean checkCameraHardware(Context context) {
@@ -51,8 +71,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void openCamera() {
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
+            }
+            catch (IOException ex)
+            {
+
+            }
+            if(photoFile != null)
+            {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        }
+
     }
 
     @Override
@@ -60,5 +98,20 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
     }
 
+    String mCurrentPhotoPath;
 
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 }
